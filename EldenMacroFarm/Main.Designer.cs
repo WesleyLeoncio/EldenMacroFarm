8cf53146bb3fa996dfaf0eb65a0ac06464cdbad9
Your Name <wesley.leoncio.izi@gmail.com>
2025-03-05 17:12:26 -0300

    first commit

---
+++ EldenMacroFarm/Main.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace EldenMacroFarm
+{
+    partial class Main
+    {
+        // Constantes para os layouts e dimensões
+        private const int LabelX = 12;
+        private const int InputX = 100;
+        private const int ButtonX = 190;
+        private const int Row1Y = 12;
+        private const int Row2Y = 45;
+        private const int RowSpacing = 10;  // Espaço entre as linhas
+        private const int ControlWidth = 80;
+        private const int ControlHeight = 23;
+        private const int ButtonWidth = 80;
+        private const int ButtonHeight = 28;
+        private const int TopOffset = 35;
+
+        private System.ComponentModel.IContainer components = null;
+        private NumericUpDown numericUpDownLoops;
+        private NumericUpDown numericLoadDelay;
+        private Button btnStart;
+        private Button btnStop;
+        private Label lblNumeroR;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+
+            ConfigJanela();
+            
+            // Criando os componentes
+            var lblLoops = CreateLabel("Repetições:", LabelX, Row1Y + TopOffset);
+            numericUpDownLoops = CreateNumericUpDown(InputX + 10, Row1Y + TopOffset, 1, 9999, 1);
+
+            var lblDelay = CreateLabel("Load (ms):", LabelX, Row2Y + RowSpacing + TopOffset);
+            numericLoadDelay = CreateNumericUpDown(InputX + 10, Row2Y + RowSpacing + TopOffset, 1, 99999, 10000);
+            
+            lblNumeroR = CreateLabel("Número de Execuções: 0", (this.ClientSize.Width / 2) - 75, 10);
+
+            // Botões
+            btnStart = CreateButton("Iniciar", ButtonX + 10, Row1Y + TopOffset);
+            btnStart.Click += new EventHandler(this.btnStart_Click);
+
+            btnStop = CreateButton("Parar", ButtonX + 10, Row2Y + RowSpacing + TopOffset);
+            btnStop.Click += new EventHandler(this.btnStop_Click);
+            btnStop.Enabled = false;
+
+            // Alterando as cores dos controles
+            SetControlColors();
+
+            // Adicionando os componentes ao formulário
+            this.Controls.Add(lblLoops);
+            this.Controls.Add(numericUpDownLoops);
+            this.Controls.Add(lblDelay);
+            this.Controls.Add(lblNumeroR);
+            this.Controls.Add(numericLoadDelay);
+            this.Controls.Add(btnStart);
+            this.Controls.Add(btnStop);
+
+            // Configurações do formulário
+            this.ClientSize = new System.Drawing.Size(290, 150);  // Aumentando o tamanho do formulário para acomodar os controles
+            this.Text = "Macro Elden Ring";
+
+            // Centralizando o formulário na tela
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+
+            // Impedindo o redimensionamento da janela
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false; // Desabilita o botão de maximizar
+            this.MinimizeBox = false; // Desabilita o botão de minimizar
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        // Método para criar Label com estilo
+        private Label CreateLabel(string text, int x, int y)
+        {
+            return new Label
+            {
+                Text = text,
+                Location = new System.Drawing.Point(x, y),
+                AutoSize = true,
+                Font = new Font("Comic Sans MS", 10, FontStyle.Italic | FontStyle.Bold),
+                ForeColor = System.Drawing.Color.White,
+                BackColor = Color.FromArgb(128, 0, 0, 0)
+            };
+        }
+
+        // Método para criar NumericUpDown com estilo
+        private NumericUpDown CreateNumericUpDown(int x, int y, int min, int max, int value)
+        {
+            return new NumericUpDown
+            {
+                Location = new System.Drawing.Point(x, y),
+                Minimum = min,
+                Maximum = max,
+                Value = value,
+                Size = new System.Drawing.Size(ControlWidth, ControlHeight),
+                Font = new System.Drawing.Font("Comic Sans MS", 10, System.Drawing.FontStyle.Bold),
+                BackColor = Color.Indigo,
+                ForeColor = Color.White
+            };
+        }
+
+        // Método para criar Button com estilo
+        private Button CreateButton(string text, int x, int y)
+        {
+            Button button = new Button
+            {
+                Text = text,
+                Location = new System.Drawing.Point(x, y),
+                Size = new System.Drawing.Size(ButtonWidth, ButtonHeight),
+                Font = new System.Drawing.Font("Comic Sans MS", 10, System.Drawing.FontStyle.Bold)
+            };
+            button.MouseEnter += (s, e) => button.Cursor = Cursors.Hand; 
+            button.MouseLeave += (s, e) => button.Cursor = Cursors.Default;
+            return button;
+        }
+
+        // Método para ajustar as cores do formulário
+        private void SetControlColors()
+        {
+            btnStart.BackColor = System.Drawing.Color.Green; // Cor do fundo do botão Iniciar
+            btnStart.ForeColor = System.Drawing.Color.White; // Cor do texto do botão Iniciar
+            btnStop.BackColor = System.Drawing.Color.Red; // Cor do fundo do botão Parar
+            btnStop.ForeColor = System.Drawing.Color.White; // Cor do texto do botão Parar
+        }
+        private void BackGroundImage()
+        {
+            Console.WriteLine(Path.GetDirectoryName("img"));
+            this.BackgroundImage = Image.FromFile(Application.StartupPath + @"\img\background.png");
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void ConfigJanela()
+        {
+            BackGroundImage();
+            this.Icon = new Icon(Application.StartupPath + @"\img\icone.ico");
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,165 @@
+using System.Runtime.InteropServices;
+using WindowsInput;
+using WindowsInput.Events;
+
+namespace EldenMacroFarm;
+
+public partial class Main : Form
+{
+    private CancellationTokenSource? _cts;
+    private int _executionCount;
+    
+    protected override void OnHandleCreated(EventArgs e)
+    {
+        base.OnHandleCreated(e);
+        DefinirCorBarraTitulo(Color.DarkSlateBlue);
+    }
+    
+    [DllImport("dwmapi.dll")]
+    private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+    // Importa funções do Windows para ativar a janela do jogo
+    [DllImport("user32.dll")]
+    private static extern IntPtr FindWindow(string? lpClassName, string lpWindowName);
+
+    [DllImport("user32.dll")]
+    private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    public Main()
+    {
+        InitializeComponent();
+    }
+
+    private async void btnStart_Click(object sender, EventArgs e)
+    {
+        if (_cts != null) return; // Evita múltiplas execuções
+
+        int loops = (int)numericUpDownLoops.Value;
+        int loadDelay = (int)numericLoadDelay.Value;
+        
+        if (loops <= 0)
+        {
+            MessageBox.Show("Escolha um número válido de loops!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return;
+        }
+
+        _cts = new CancellationTokenSource();
+        btnStart.Enabled = false;
+        btnStop.Enabled = true;
+
+        await RunMacro(loops, loadDelay, _cts.Token);
+
+        btnStart.Enabled = true;
+        btnStop.Enabled = false;
+        _cts.Dispose();
+        _cts = null;
+    }
+
+    private void btnStop_Click(object sender, EventArgs e)
+    {
+        _cts?.Cancel();
+    }
+
+    private async Task RunMacro(int loops, int loaDelay, CancellationToken token)
+    {
+        for (int i = 0; i < loops; i++)
+        {
+            if (AtualizarBotao(token)) return;
+
+            // Ativa a janela do Elden Ring
+            IntPtr hWnd = FindWindow(null, "ELDEN RING™");
+            if (hWnd != IntPtr.Zero)
+                SetForegroundWindow(hWnd);
+            
+            await GerarDelay(300);
+            
+            await AcionarTecla(KeyCode.G, 150, 600, token);
+            
+            await AcionarTecla(KeyCode.W, 3000, 100, token);
+            
+            await AcionarTecla(KeyCode.A, 500, 100, token);
+            
+            await AcionarTecla(KeyCode.W, 2000, 100, token);
+            
+            await AcionarTecla(KeyCode.A, 490, 100, token);
+            
+            await AcionarTecla(KeyCode.W, 1000, 100, token);
+            
+            await AcionarTecla(KeyCode.F, 100, 6000, token);
+            
+            await AcionarTecla(KeyCode.M, 5, 100, token);
+            
+            await AcionarTecla(KeyCode.S, 5, 100, token);
+            
+            for (int j = 0; j < 4; j++)
+            {
+                await AcionarTecla(KeyCode.E, 100, 200, token);
+            }
+
+            await LiberaTeclas();
+            UpdateCounter();
+            await GerarDelay(loaDelay);
+        }
+        AtualizarBotao(token);
+    }
+    
+    private void UpdateCounter()
+    {
+        _executionCount++;
+        lblNumeroR.Text = $"Número de Execuções: {_executionCount}";
+    }
+
+    private bool AtualizarBotao(CancellationToken token)
+    {
+        if (token.IsCancellationRequested)
+        {
+            btnStop.Enabled = false;
+            btnStart.Enabled = true;
+            return true;
+        }
+        return false;
+    }
+
+    private async Task AcionarTecla(KeyCode keyCode, int wat, int delay, CancellationToken token)
+    {
+        if (AtualizarBotao(token)) return;
+        
+        await Simulate.Events().Hold(keyCode).Wait(wat).Release().Invoke();
+        await GerarDelay(delay);
+        await LiberaTeclasManualmente(keyCode);
+        await GerarDelay(50);
+    }
+
+    private static async Task LiberaTeclasManualmente(KeyCode keyCode)
+    {
+        await Simulate.Events().Release(keyCode).Invoke(); // 🔹 Libera novamente
+        await GerarDelay(50);
+    }
+
+    private static async Task LiberaTeclas()
+    {
+        await Simulate.Events()
+            .Release(KeyCode.W)
+            .Release(KeyCode.A)
+            .Release(KeyCode.F)
+            .Release(KeyCode.G)
+            .Release(KeyCode.M)
+            .Release(KeyCode.S)
+            .Release(KeyCode.E)
+            .Invoke();
+    }
+
+    private static async Task GerarDelay(int tempo)
+    {
+        if (tempo <= 0) await Task.Delay(100);
+
+        await Task.Delay(tempo);
+    }
+    
+    private void DefinirCorBarraTitulo(Color cor)
+    {
+        int attr = 35; // DWMWA_CAPTION_COLOR
+        int color = cor.R | (cor.G << 8) | (cor.B << 16);
+        DwmSetWindowAttribute(this.Handle, attr, ref color, sizeof(int));
+    }
+}
